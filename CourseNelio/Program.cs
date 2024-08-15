3c1726157f454d2b13fe1dca8a9b490c1e62936d
Lucas Assis <lucasrafaeisaac@gmail.com>
2024-08-15 15:16:20 -0300

    initial

---
+++ CourseNelio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+            sbyte numero = 120;
+
+            byte n1 = 255;
+            n1 ++;
+            Console.WriteLine(n1);
+
+            int n2 = 10000;
+            Console.WriteLine(n2);
+            
+            long n3 = 9223;
+
+            Console.WriteLine(n3);
+
+           
+            for (int i = 0; i < numero; i+=10)
+            {
+                Console.WriteLine("Isso" + numero + i);
+                
+            }
+
+            
+            
+        }
+    }
+}
